95f082aecf592069708b5d52eaa13bc36b93d363
François Bonnefont <happypoulp@gmail.com>
2016-01-22 23:17:20 +0100

    Reduce boilerplate

--- src/index.js
+++ src/index.js
@@ -14,46 +14,31 @@ const dataList = [
   { id: 2, text: 'two', }
 ]
 
-/**************************************** ACTIONS *************************************/
+/**************************************** ACTION-CREATOR *************************************/
 
 const listThunk = () => (dispatch) => {
   dispatch({ type: 'LIST_REQUEST' })
 
-  setTimeout(() => dispatch({ type: 'LIST_SUCCESS', response: dataList }), 200)
+  setTimeout(
+    () => dispatch({ type: 'LIST_SUCCESS', response: dataList }),
+    200
+  )
 }
 
 /**************************************** COMPONENTS *************************************/
 
 class AppContainer extends Component {
-  static fetchData(dispatch) {
-    console.log('AppContainer fetchData')
-    dispatch(listThunk())
-  }
-
   componentDidMount () {
     console.log('componentDidMount')
-    if (!this.props.other) {
-      this.constructor.fetchData(this.props.dispatch)
+    if (!this.props.list) {
+      this.props.dispatch(listThunk())
     }
   }
 
   componentWillReceiveProps (nextProps) {
     console.log('componentWillReceiveProps', 'id', this.props.params.id, 'nextId', nextProps.params.id)
     if (this.props.params.id !== nextProps.params.id) {
-      this.constructor.fetchData(this.props.dispatch)
-    }
-  }
-
-  renderList (list) {
-    if (list) {
-      return list.map(item =>
-        <li key={ item.id }>
-          <Link to={`/${item.id}`}>{ item.text }</Link>
-        </li>
-      )
-    }
-    else {
-      return null
+      this.props.dispatch(listThunk())
     }
   }
 
@@ -61,37 +46,26 @@ class AppContainer extends Component {
     console.log('AppContainer render')
 
     return <ul>
-      { this.renderList(this.props.app.list) }
+      { this.props.app.list && this.props.app.list.map(item =>
+        <li key={ item.id }>
+          <Link to={`/${item.id}`}>{ item.text }</Link>
+        </li>
+      ) }
     </ul>
 
   }
 }
-const ConnectedAppContainer = connect((state) => {
-  return {
-    app: state.app
-  }
-})(AppContainer)
-
+const ConnectedAppContainer = connect(state => ({ app: state.app }))(AppContainer)
 
 /**************************************** REDUCER *************************************/
 
-const initialState = {
-  list: null,
-}
+const initialState = { list: null }
 
 const appReducer = (state = initialState, action) => {
   console.log('-- appReducer', state, action)
   switch (action.type) {
-    case 'LIST_REQUEST':
-      return {
-        list: null
-      }
-    break;
-    case 'LIST_SUCCESS':
-      return {
-        list: action.response
-      }
-    break;
+    case 'LIST_REQUEST': return { list: null }
+    case 'LIST_SUCCESS': return { list: action.response }
   }
   return state
 }
