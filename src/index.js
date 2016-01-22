4e317f67c2240ac25417581243631faea278aaf7
François Bonnefont <happypoulp@gmail.com>
2016-01-22 23:03:47 +0100

    Remove non needed code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Route, Router, Link } from 'react-router'
 import createHistory from 'history/lib/createHashHistory'
 import { reduxReactRouter, routerStateReducer, ReduxRouter } from 'redux-router'
 import thunk from 'redux-thunk'
-import Immutable from 'immutable'
 
 /**************************************** FAKE DATA *************************************/
 
@@ -17,29 +16,18 @@ const dataList = [
 
 /**************************************** ACTIONS *************************************/
 
-const listThunk = () => {
-  return (dispatch, getState) => {
-    dispatch({
-      type: 'LIST_REQUEST'
-    })
+const listThunk = () => (dispatch) => {
+  dispatch({ type: 'LIST_REQUEST' })
 
-    setTimeout(() => {
-      return dispatch({
-        type: 'LIST_SUCCESS',
-        response: dataList
-      })
-    }, 200)
-  }
+  setTimeout(() => dispatch({ type: 'LIST_SUCCESS', response: dataList }), 200)
 }
 
 /**************************************** COMPONENTS *************************************/
 
-const listAction = listThunk
-
 class AppContainer extends Component {
   static fetchData(dispatch) {
     console.log('AppContainer fetchData')
-    dispatch(listAction())
+    dispatch(listThunk())
   }
 
   componentDidMount () {
@@ -56,74 +44,53 @@ class AppContainer extends Component {
     }
   }
 
+  renderList (list) {
+    if (list) {
+      return list.map(item =>
+        <li key={ item.id }>
+          <Link to={`/${item.id}`}>{ item.text }</Link>
+        </li>
+      )
+    }
+    else {
+      return null
+    }
+  }
+
   render () {
     console.log('AppContainer render')
-    const other = this.props.app.get('other')
-    const list = this.props.app.get('list')
 
-    return <div>
-      {
-        React.cloneElement(
-          this.props.children,
-          {
-            ...this.props,
-            list: list,
-            children: null
-          }
-        )
-      }
-    </div>
+    return <ul>
+      { this.renderList(this.props.app.list) }
+    </ul>
+
   }
 }
 const ConnectedAppContainer = connect((state) => {
-  console.log('connect AppContainer', state.app.get('list'), state.app.get('other'))
   return {
     app: state.app
   }
 })(AppContainer)
 
-class App extends Component {
-
-  renderList (list) {
-    if (list) {
-      return list.map(item => {
-        return <li key={ item.get('id') }>
-          <Link to={`/${item.get('id')}`}>{ item.get('text') }</Link>
-        </li>
-      })
-    }
-  }
-
-  render () {
-    const { other, list } = this.props
-    console.log('@@@@@@@ App this.props', this.props)
-
-    let listComp = null
-
-    if (list) {
-      listComp = this.renderList(list)
-    }
-
-    return <ul>
-      { listComp }
-    </ul>
-  }
-}
 
 /**************************************** REDUCER *************************************/
 
-const initialState = Immutable.fromJS({
+const initialState = {
   list: null,
-})
+}
 
 const appReducer = (state = initialState, action) => {
   console.log('-- appReducer', state, action)
   switch (action.type) {
     case 'LIST_REQUEST':
-      return state.set('list', null)
+      return {
+        list: null
+      }
     break;
     case 'LIST_SUCCESS':
-      return state.set('list', Immutable.fromJS(action.response))
+      return {
+        list: action.response
+      }
     break;
   }
   return state
@@ -131,9 +98,7 @@ const appReducer = (state = initialState, action) => {
 
 /**************************************** ROUTES *************************************/
 
-const routes = <Route path="/" component={ConnectedAppContainer}>
-  <Route path=":id" component={App} />
-</Route>
+const routes = <Route path="/:id" component={ConnectedAppContainer} />
 
 /**************************************** STORE CONFIGURATION *************************************/
 
