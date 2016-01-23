d0717fce7f188896c4b98305ad8a6405f6760d26
François Bonnefont <happypoulp@gmail.com>
2016-01-23 01:23:10 +0100

    Remove useless deps

--- webpack.config-dev.js
+++ webpack.config-dev.js
@@ -1,22 +1,7 @@
 import webpack from 'webpack'
 import path from 'path'
 
-var babelSettings = { stage: 0 }
-
-babelSettings.plugins = ['react-transform:after']
-babelSettings.extra = {
-  'react-transform': {
-    transforms: [{
-      transform: 'react-transform-hmr',
-      imports: ['react'],
-      locals: ['module']
-    }, {
-      transform: 'react-transform-catch-errors',
-      imports: ['react']
-    }]
-  }
-}
-
+const babelSettings = { stage: 0 }
 const config = {
 
   context: __dirname,
